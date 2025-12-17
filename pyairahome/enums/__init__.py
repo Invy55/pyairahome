a91da812d60be27515edff411c4013636c77aa3f
Marco <38048104+Invy55@users.noreply.github.com>
2025-12-17 11:07:22 +0100

    Update __init__.py

--- pyairahome/enums/__init__.py
+++ pyairahome/enums/__init__.py
@@ -1,5 +1,7 @@
 from ..device.heat_pump.statistics.v1.service_pb2 import Granularity
 from ..device.heat_pump.ble.v1.get_data_pb2 import DataType
+from ..device.v1.device_identifier_pb2 import DeviceType
+
 from enum import Enum
 
 # Explicitly define __all__ for static analysis
@@ -14,4 +16,9 @@ Granularity = Enum('Granularity', {
 GetDataType = Enum('GetDataType', {
     name: descriptor.number
     for name, descriptor in DataType.DESCRIPTOR.values_by_name.items()
+})
+
+DeviceType = Enum('DeviceType', {
+    name: descriptor.number
+    for name, descriptor in DeviceType.DESCRIPTOR.values_by_name.items()
 })
